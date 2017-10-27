84815d2a7881e038bca3543d3a7738995d904fcc
kanatto625 <kanatto625@gmail.com>
2017-10-27 21:03:44 +0900

    edit test for TodoComponent

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -1,25 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { ModalModule } from 'ngx-bootstrap';
+
 import { TodoComponent } from './todo.component';
+import { FirebaseService } from '../service/firebase.service';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let de: DebugElement;
+  let service: FirebaseService;
 
   beforeEach(async(() => {
+
+    let serviceStub = {
+      getItems(url: string) {},
+      addItem(item) {},
+      updateItem(item) {},
+      deleteItem(item) {}
+    };
     TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      imports: [
+        ModalModule.forRoot()
+      ],
+      declarations: [ TodoComponent ],
+      providers: [
+        { provide: FirebaseService, useValue: serviceStub }
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    service = fixture.debugElement.injector.get(FirebaseService);
+  }));
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
