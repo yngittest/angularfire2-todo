d6e89d775df9a9c7180f59f59551ef69ba05b953
kanatto625 <kanatto625@gmail.com>
2018-04-23 21:18:43 +0900

    add reminderTodo function

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
+const moment = require("moment");
 exports.onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}/done')
     .onUpdate((change, context) => {
     if (!change.after.val()) {
@@ -35,6 +36,26 @@ exports.onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}/done
         })
             .catch(err => {
             console.log(err);
+        });
+    });
+});
+exports.reminderTodo = functions.https.onRequest((req, res) => {
+    res.send('accepted');
+    const now = moment().utcOffset('+09:00');
+    return admin.database().ref(`/todos`)
+        // .orderByChild('due')
+        // .endAt(now)
+        .on('child_added', snapshots => {
+        snapshots.forEach(snapshot => {
+            const data = snapshot.val();
+            if (!data.done) {
+                const due = moment(data.due + '+09:00');
+                const diff = due.diff(now, 'minutes', true);
+                if (0 <= diff && diff < 1) {
+                    console.log(snapshot.key, data);
+                }
+            }
+            return false;
         });
     });
 });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
+import * as moment from 'moment';
+
 export const onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}/done')
   .onUpdate((change, context) => {
     if(!change.after.val()) {
@@ -39,3 +41,24 @@ export const onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}
           });
       });
   });
+
+export const reminderTodo = functions.https.onRequest((req, res) => {
+  res.send('accepted');
+  const now = moment().utcOffset('+09:00');
+  return admin.database().ref(`/todos`)
+    // .orderByChild('due')
+    // .endAt(now)
+    .on('child_added', snapshots => {
+      snapshots.forEach(snapshot => {
+        const data = snapshot.val();
+        if(!data.done) {
+          const due = moment(data.due + '+09:00');
+          const diff = due.diff(now, 'minutes', true);
+          if(0 <= diff && diff < 1) {
+            console.log(snapshot.key, data);
+          }
+        }
+        return false;
+      });
+    });
+});
