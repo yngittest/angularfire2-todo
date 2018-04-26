c586122860b9542f12f2b9bf505b120c0e857bf4
kanatto625 <kanatto625@gmail.com>
2018-04-26 21:06:10 +0900

    add reminder push

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -4,6 +4,24 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 const moment = require("moment");
+const pushNotification = (userId, title) => {
+    const payload = {
+        notification: {
+            title: title
+        }
+    };
+    admin.database().ref(`/users/${userId}/fcmToken`).once('value')
+        .then(token => token.val())
+        .then(userFcmToken => {
+        return admin.messaging().sendToDevice(userFcmToken, payload);
+    })
+        .then(res => {
+        console.log("Sent Successfully", res);
+    })
+        .catch(err => {
+        console.log(err);
+    });
+};
 exports.onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}/done')
     .onUpdate((change, context) => {
     if (!change.after.val()) {
@@ -12,26 +30,12 @@ exports.onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}/done
     return change.after.ref.parent.once('value')
         .then(snapshot => {
         const data = snapshot.val();
-        const payload = {
-            notification: {
-                title: `${data.title} completed`
-            }
-        };
-        console.log(payload);
         const groupId = context.params.groupId;
         admin.database().ref(`/groups/${groupId}/members`).once('value')
-            .then(members => Object.keys(members.val())[0])
-            .then(userId => {
-            admin.database().ref(`/users/${userId}/fcmToken`).once('value')
-                .then(token => token.val())
-                .then(userFcmToken => {
-                return admin.messaging().sendToDevice(userFcmToken, payload);
-            })
-                .then(res => {
-                console.log("Sent Successfully", res);
-            })
-                .catch(err => {
-                console.log(err);
+            .then(membersRef => Object.keys(membersRef.val()))
+            .then(memberKeys => {
+            memberKeys.forEach(userId => {
+                pushNotification(userId, `${data.title} completed`);
             });
         })
             .catch(err => {
@@ -41,18 +45,18 @@ exports.onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}/done
 });
 exports.reminderTodo = functions.https.onRequest((req, res) => {
     res.send('accepted');
-    const now = moment().utcOffset('+09:00');
     return admin.database().ref(`/todos`)
         // .orderByChild('due')
         // .endAt(now)
         .on('child_added', snapshots => {
+        const now = moment().utcOffset('+09:00');
         snapshots.forEach(snapshot => {
             const data = snapshot.val();
             if (!data.done) {
                 const due = moment(data.due + '+09:00');
                 const diff = due.diff(now, 'minutes', true);
                 if (0 <= diff && diff < 1) {
-                    console.log(snapshot.key, data);
+                    pushNotification(data.assignee, `Reminder ${data.title}`);
                 }
             }
             return false;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,25 @@ admin.initializeApp(functions.config().firebase);
 
 import * as moment from 'moment';
 
+const pushNotification = (userId, title) => {
+  const payload = {
+    notification: {
+      title: title
+    }
+  };
+  admin.database().ref(`/users/${userId}/fcmToken`).once('value')
+    .then(token => token.val() )
+    .then(userFcmToken => {
+      return admin.messaging().sendToDevice(userFcmToken, payload);
+    })
+    .then(res => {
+      console.log("Sent Successfully", res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 export const onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}/done')
   .onUpdate((change, context) => {
     if(!change.after.val()) {
@@ -13,28 +32,13 @@ export const onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}
     return change.after.ref.parent.once('value')
       .then(snapshot => {
         const data = snapshot.val();
-        const payload = {
-          notification: {
-            title: `${data.title} completed`
-          }
-        };
-        console.log(payload);
-
         const groupId  = context.params.groupId;
         admin.database().ref(`/groups/${groupId}/members`).once('value')
-          .then(members => Object.keys(members.val())[0])
-          .then(userId => {
-            admin.database().ref(`/users/${userId}/fcmToken`).once('value')
-              .then(token => token.val() )
-              .then(userFcmToken => {
-                return admin.messaging().sendToDevice(userFcmToken, payload);
-              })
-              .then(res => {
-                console.log("Sent Successfully", res);
-              })
-              .catch(err => {
-                console.log(err);
-              });
+          .then(membersRef => Object.keys(membersRef.val()))
+          .then(memberKeys => {
+            memberKeys.forEach(userId => {
+              pushNotification(userId, `${data.title} completed`);
+            });
           })
           .catch(err => {
             console.log(err);
@@ -44,18 +48,18 @@ export const onTodoCompleted = functions.database.ref('/todos/{groupId}/{todoId}
 
 export const reminderTodo = functions.https.onRequest((req, res) => {
   res.send('accepted');
-  const now = moment().utcOffset('+09:00');
   return admin.database().ref(`/todos`)
     // .orderByChild('due')
     // .endAt(now)
     .on('child_added', snapshots => {
+      const now = moment().utcOffset('+09:00');
       snapshots.forEach(snapshot => {
         const data = snapshot.val();
         if(!data.done) {
           const due = moment(data.due + '+09:00');
           const diff = due.diff(now, 'minutes', true);
           if(0 <= diff && diff < 1) {
-            console.log(snapshot.key, data);
+            pushNotification(data.assignee, `Reminder ${data.title}`);
           }
         }
         return false;
