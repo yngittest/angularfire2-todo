abf2569b8ed99e6077a507bdeadcdcd35ecdc7ef
kanatto625 <kanatto625@gmail.com>
2018-05-24 16:15:34 +0900

    don't filter todo which assignee is null

--- src/app/pipe/filter-by/filter-by.pipe.ts
+++ src/app/pipe/filter-by/filter-by.pipe.ts
@@ -7,9 +7,13 @@ export class FilterByPipe implements PipeTransform {
 
   transform(array: any[], field: string, value: any): any[] {
     if(array && array.length > 0) {
-      array = array.filter(element => {
+      const my = array.filter(element => {
         return element[field] === value;
       });
+      const no = array.filter(element => {
+        return element[field] === null;
+      });
+      array = my.concat(no);
     }
     return array;
   }
